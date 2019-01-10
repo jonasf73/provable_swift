4b924f4580ace72498f98aae429b6fa4071e0ead
jjonas73 <github@dnsupdate.org>
2019-01-10 12:21:02 +0100

    - fix typos - rephrase some texts - add more theorems

--- Proofs.playground/Contents.swift
+++ Proofs.playground/Contents.swift
@@ -1,5 +1,5 @@
 //: ## IF IT BUILDS, IT'S PROVEN!
-//: *(only rule: don't use the standard lib)*
+//: *(you are not allowed to use the standard lib)*
 //:
 //: Proving a "proposition" `P` is equivalent to writing some code that is able to create an element of type `P`.
 //:
@@ -13,6 +13,7 @@
 //: func f(_: P) -> Q {}
 //: ```
 //: then, you "proved" `P ⟹ Q` since you can construct an element of `Q` from any element of `P` using this function.
+//: Or rephrased in logical terms: if you have a proof of `P`, you get a proof of `Q`.
 //:
 //: **Note** that the actual implementation is irrelevant as long as it compiles.
 //: Same as a proof: how long, unreadable or inneficient it is, if it is correct, the important thing is that the result has been proven.
@@ -48,7 +49,12 @@ func forget<P,Q>() -> IMP<P,IMP<Q,P>> {
     return { p in { q in p }}
 }
 
-//: ### Syllogisms
+//: `∀ P Q, Q ⟹ P ⟹ P`
+func forget2<P,Q>() -> IMP<Q,IMP<P,P>> {
+    return { q in trivial }
+}
+
+//: ### Modus ponens
 //: `∀ P Q: P ⟹ (P ⟹ Q) ⟹ Q`
 func induction<P,Q>() -> IMP<P,IMP<IMP<P,Q>,Q>> {
     return { p in { pq in pq(p) }}
@@ -64,10 +70,12 @@ func imp_comm<P,Q,R>() -> IMP<IMP<P,IMP<Q,R>>,IMP<Q,IMP<P,R>>> {
     return { pqr in { q in { p in pqr(p)(q) }}}
 }
 
+//: ### Ex falso, quodlibet
 //: If you can prove False, you can prove anything!
+//:
 //: `∀ P, False -> P`
 func False_proves_anything<P>() -> ((False) -> P) {
-    return { ff in switch ff { } }
+    return { non_existing_element_of_False in switch non_existing_element_of_False { } }
 }
 
 //: ### Not construct
@@ -80,6 +88,13 @@ typealias NOT<P> = IMP<P,False>
 //: **Note** that proving `~~P ⟹ P` requires `P` to be decidable.
 func P_imp_not_not_P<P>() -> IMP<P,NOT<NOT<P>>> {
     return { p in { np in np(p) }}
+}
+
+//: `∀ P Q, (P ⟹ Q) ⟹ (~Q ⟹ ~P)`
+//:
+//: **Note** that the equivalence only works if `Q` is decidable.
+func contrapositive_right<P,Q>() -> IMP<IMP<P,Q>,IMP<NOT<Q>,NOT<P>>>  {
+    return { impPQ in { notQ in { p in notQ(impPQ(p)) } } }
 }
 
 //: ### Reductio ad absurdum
@@ -145,7 +160,6 @@ func or_assoc_left<P,Q,R>() -> IMP<OR<OR<P,Q>,R>,OR<P,OR<Q,R>>> {
     }
 }
 
-
 //: `∀ P Q R, P ∨ (Q ∨ R) ⟹ (P ∨ Q) ∨ R`
 func or_assoc_right<P,Q,R>() -> IMP<OR<P,OR<Q,R>>,OR<OR<P,Q>,R>> {
     // just to test the whole thing, let's do some proof based only on Swift typesystem and previous proofs
@@ -168,12 +182,17 @@ func P_or_P_imp_P<P>() -> IMP<OR<P,P>,P> {
     }
 }
 
+//: `∀ P Q, ~P -> ~Q -> ~(P ∨ Q)`
+func not_not_or<P,Q>() -> IMP<NOT<P>,IMP<NOT<Q>,NOT<OR<P,Q>>>> {
+    return { notP in { notQ in { orPQ in switch orPQ { case .left(let p): return notP(p); case .right(let q): return notQ(q) }}}}
+}
+
 //: ### And construct
 typealias AND<P,Q> = (P,Q)
 
 //: `∀ P Q, P ⟹ Q ⟹ P ∧ Q`
 func conj<P,Q>() -> IMP<P,IMP<Q,AND<P,Q>>> {
-    return { p in return { q in (p,q) }}
+    return { p in { q in (p,q) }}
 }
 
 //: `∀ P Q, P ∧ Q ⟹ P`
@@ -188,7 +207,7 @@ func split_and_right<P,Q>() -> IMP<AND<P,Q>,Q> {
 
 //: `∀ P Q R, (P ⟹ Q) ⟹ (P ⟹ R) ⟹ (P ⟹ P ∧ R)`
 func and_join<P,Q,R>() -> IMP<IMP<P,Q>,IMP<IMP<P,R>,IMP<P,AND<Q,R>>>> {
-    return { pq in { pr in { p in return (pq(p), pr(p)) }}}
+    return { pq in { pr in { p in (pq(p), pr(p)) }}}
 }
 
 //: `∀ P Q, P ∧ Q ⟹ Q ∧ P`
@@ -198,7 +217,7 @@ func and_comm<P,Q>() -> IMP<AND<P,Q>,AND<Q,P>> {
 
 //: `∀ P Q R, (P ⟹ Q) ⟹ (P ∧ R ⟹ Q ∧ R)`
 func imp_compat_with_and<P,Q,R>() -> IMP<IMP<P,Q>,IMP<AND<P,R>,AND<Q,R>>> {
-    return { pq in return and_join()(compose(pq, split_and_left()))(split_and_right()) }
+    return { pq in and_join()(compose(pq, split_and_left()))(split_and_right()) }
 }
 
 //: `∀ P Q R, (P ∧ Q) ∧ R ⟹ P ∧ (Q ∧ R)`
@@ -213,6 +232,15 @@ func and_assoc_right<P,Q,R>() -> IMP<AND<P,AND<Q,R>>,AND<AND<P,Q>,R>> {
     return and_join()(and_join()(split_and_left())(compose(split_and_left(), split_and_right())))(compose(split_and_right(), split_and_right()))
 }
 
+//: `∀ P Q, ~P ⟹ ~(P ∧ Q)`
+func not_P_imp_not_P_and_Q<P,Q>() -> IMP<NOT<P>,NOT<AND<P,Q>>> {
+    return { notP in { pAndQ in notP(pAndQ.0) } }
+}
+
+//: `∀ P Q, ~Q ⟹ ~(P ∧ Q)`
+func not_Q_imp_not_P_and_Q<P,Q>() -> IMP<NOT<Q>,NOT<AND<P,Q>>> {
+    return { notQ in { pAndQ in notQ(pAndQ.1) } }
+}
 //: Rephrasing imp_trans theorem using ∧
 //:
 //: `∀ P Q R, (P ⟹ Q) ∧ (Q ⟹ R) ⟹ (P ⟹ R)`
@@ -280,15 +308,76 @@ func not_not_P_imp_P<P>() -> IMP<DECIDABLE<P>,IMP<NOT<NOT<P>>,P>> {
     }
 }
 
+//: `∀ P Q, P is decidable ⟹ Q is decidable ⟹ P ∨ Q is decidable`
+func decidable_compat_or<P,Q>() -> IMP<DECIDABLE<P>,IMP<DECIDABLE<Q>, DECIDABLE<OR<P,Q>>>> {
+    return { dp in
+        { dq in
+            switch (dp) {
+            case .left(let p):
+                return .left(.left(p))
+            case .right(let notP):
+                switch dq {
+                case .left(let q):
+                    return .left(.right(q))
+                case .right(let notQ):
+                    return .right(not_not_or()(notP)(notQ))
+                }
+            }
+        }
+    }
+}
+
+//: `∀ P Q, P is decidable ⟹ Q is decidable ⟹ P ∧ Q is decidable`
+func decidable_compat_and<Q,P>() -> IMP<DECIDABLE<P>,IMP<DECIDABLE<Q>, DECIDABLE<AND<P,Q>>>> {
+    return { dp in
+        { (dq : DECIDABLE<Q>) in
+            switch(dp) {
+            case .left(let p):
+                switch dq {
+                case .left(let q):
+                    return .left((p , q))
+                case .right(let notQ):
+                    return .right(not_Q_imp_not_P_and_Q()(notQ))
+                }
+            case .right(let notP):
+                return .right(not_P_imp_not_P_and_Q()(notP))
+            }
+        }
+    }
+}
+
+//: `∀ P Q, (Q is decidable) ⟹ (~Q ⟹ ~P) ⟹ (P ⟹ Q)`
+func contrapositive_left<P,Q>() -> IMP<DECIDABLE<Q>,IMP<IMP<NOT<Q>,NOT<P>>,IMP<P,Q>>>  {
+    return { decQ in
+        { not_Q_imp_not_P in
+            { p in
+                switch (decQ) {
+                case .left(let q):
+                    return q
+                case .right(let notQ):
+                    // not_Q_imp_not_P(notQ)(p) never returns
+                    not_Q_imp_not_P(notQ)(p)
+                }
+            }
+        }
+    }
+}
+
+//: `∀ P Q, (Q is decidable) ⟹ (~Q ⟹ ~P) ⟺ (P ⟹ Q)`
+func contrapositive<P,Q>() -> IMP<DECIDABLE<Q>,IFF<IMP<NOT<Q>,NOT<P>>,IMP<P,Q>>>  {
+    return { decQ in (contrapositive_left()(decQ), contrapositive_right()) }
+}
+
 //: ### Beyond Swift Type System
 //: At this point, the Swift Type System is pretty limited. To express more complex propositionts you need an elaborate type system where types can be dependent.
 //:
 //: Swift Type System only allows dependent type through generics which act as universal quantifiers.
 //:
-//: Also there is one tiny litte **second rule** to the game: though the implementation does not really matter, still **it must** end**.
+//: Also there is one tiny litte **hidden rule** to the game: though the implementation does not really matter, still **it must end** (though not necessarily return!).
 //:
-//: You can't write functions that run forever. Alas (of course), the Swift compiler can't enforce this as this can't be decided. 😔
-//: That's why real proof assistants enforces rules on what "programs" you can't write that guarantees that they will always end.
+//: You can't write functions that run forever. Alas, the Swift compiler can't enforce this as this is famously undecidable. 😔
+//:
+//: That's why real proof assistants enforce rules on what "programs" you can write that guarantees that they will always end.
 //:
 //: With a fully Turing complete language like Swift, you can do dirty tricks like:
 //:
@@ -301,12 +390,41 @@ func not_not_P_imp_P<P>() -> IMP<DECIDABLE<P>,IMP<NOT<NOT<P>>,P>> {
 //: though this particular trick is detected by the Swift compiler which emits a warning.
 //:
 //: "Fortunately", this one does not:
+//:
+//: ```swift
+//: func everything_is_possible<P>() -> P {
+//:     while(true) {
+//:     }
+//: }
+//: ```
+//:
+//: We can use this quirk to create "axioms":
 
-func everything_is_possible<P>() -> P {
+func axiom<P>() -> P {
     while(true) {
     }
 }
 
-//: #### so yes, anything is possible!
-
-
+//: But with bad chosen axioms, you can create inconsistent theories.
+//:
+//: "*With great power…*"
+//:
+//: Still, you can use axioms to create specific theories:
+//: ### Classical Logic
+func classical_logic() {
+    
+//: *Excluded middle* axiom: all propositions are decidable
+    func excluded_middle<P>() -> DECIDABLE<P> {
+        return axiom()
+    }
+    
+//: `~~P ⟹ P`
+    func not_not_P_imp_P_classical<P>() -> IMP<NOT<NOT<P>>,P> {
+        return not_not_P_imp_P()(excluded_middle())
+    }
+    
+//: `∀ P Q, (~Q ⟹ ~P) ⟺ (P ⟹ Q)`
+    func contrapositive_classical<P,Q>() -> IFF<IMP<NOT<Q>,NOT<P>>,IMP<P,Q>>  {
+        return contrapositive()(excluded_middle())
+    }
+}
